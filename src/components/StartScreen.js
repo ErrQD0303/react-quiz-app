cda3280540647fe43f8c38d5ccb954109752b5e8
ErrQD0303 <quocdat3396@gmail.com>
2024-06-21 14:22:47 +0700

    First Version

---
+++ src/components/StartScreen.js
@@ -0,0 +1,19 @@
+function StartScreen({ numQuestions, dispatch }) {
+  return (
+    <div className="start">
+      <h2>Welcome to The React Quiz!</h2>
+      <h3>
+        {numQuestions} question{numQuestions > 1 && "s"} to test your React
+        masterry
+      </h3>
+      <button
+        className="btn btn-ui"
+        onClick={() => dispatch({ type: "start" })}
+      >
+        Let's start
+      </button>
+    </div>
+  );
+}
+
+export default StartScreen;
